f220b75a6a746b83854053e980f23d59ab2f7729
asuma312 <39178303+asuma312@users.noreply.github.com>
2025-04-27 13:20:03 -0300

    Fixed type with the specific value on enum

--- Typing.lua
+++ Typing.lua
@@ -1,10 +1,15 @@
 Typing = {}
-function Typing.VerifyEnum(table,key)
-    -- Check if the value is in the enum table
-    if table[key] then
-        return key
+function Typing.VerifyEnum(table, input)
+    if table[input] then
+        return input
     end
+    
+    for k, v in pairs(table) do
+        if v == input then
+            return input
+        end
+    end
+    
     return nil
 end
-
 return Typing
